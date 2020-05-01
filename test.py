e18b6131ab9aac13d2416341f1fcb9ef5b8f30b2
RexYing <rex.ying0923@gmail.com>
2020-05-01 16:22:39 -0700

    test printing

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import orca
 import networkx as nx
 
-print(orca.orbit_counts("node", 5, nx.gnm_random_graph(30, 100)))
+counts = orca.orbit_counts("node", 5, nx.gnm_random_graph(30, 100))
+print(f'Number of nodes: {len(counts)}')
+print(f'Number of graphlets: {len(counts[0])}')
+print('Counts: ', counts)
